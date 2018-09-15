9bccac75c7102e2469cfdc3e98a3d6d545311a9a
gtsofa <jlsnyule@yahoo.com>
2018-09-15 14:16:31 +0300

    [bug #160414179] Fix sign up Fix errors sign up form can send data to api

--- ui/js/main.js
+++ ui/js/main.js
@@ -1,9 +1,9 @@
-// Create user sign up
-document.getElementById('sign_up').addEventListener('submit', createUser);
+// eventListener to handle submit button on sign up form
+document.getElementById('signUp').addEventListener('submit', signUp);
 
-// create createUser function
-function createUser(){
-    let fullname = document.getElementById('fullname').value;
+
+// create sign up function
+function signUp(){
     let username = document.getElementById('username').value;
     let email = document.getElementById('email').value;
     let password = document.getElementById('password').value;
@@ -12,24 +12,29 @@ function createUser(){
     fetch('https://gtsofastackoverflowlite.herokuapp.com/api/v2/auth/register', {
         method: 'POST',
         headers: {
-            'Accept': 'application/json, text/plain, */*',
+            'Accept': 'application/json',
             'Content-type': 'application/json'
         },
-        body: JSON.stringify({fullname:fullname, username:username, email:email, password:password, 
+        body: JSON.stringify({username:username, email:email, password:password, 
             confirm_password:confirm_password})
     })
-    .then((respon) => respon.json())
-    .then((data) => {
-        // if response data is not defined
-        if (data.respon != undefined){
-            document.getElementById('output').style.color = 'red'
-            document.getElementById('output').innerHTML = data.respon
-        }
-        // if response data is successfully
-        if (data.respon === 'User has been created successfully'){
-            window.location.href = 'log_in.html'
-        }
+    .then((respon) => {
+        status_code = respon.status
+        return respon.json()
     })
+    .then((data) => {
+        // if response data is success
+        if (data.status_code === 200){
+            alert(respon.Message)
+            window.location.href = 'log_in.html';
+        }
+        // if response data is not success
+        if (data.status_code == 400){
+            alert(respon.Message)
+        }
+        console.log(respon.Message)
+    })
+    .catch((err)=> console.log(err))
 
     
 }
