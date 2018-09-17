eca25ce0aa7fa138d131e13443e1988f245db86b
gtsofa <jlsnyule@yahoo.com>
2018-09-18 00:17:05 +0300

    refactor sign up

--- ui/js/main.js
+++ ui/js/main.js
@@ -1,52 +1,38 @@
 // eventListener to handle submit button on sign up form
 document.getElementById('signUp').addEventListener('submit', signUp);
+// document.getElementById('logIn').addEventListener('submit', logIn);
 
 
 
 // create sign up function
-function signUp(){
+function signUp(event){
+    event.preventDefault();
     let username = document.getElementById('username').value;
     let email = document.getElementById('email').value;
     let password = document.getElementById('password').value;
     let confirm_password = document.getElementById('confirm_password').value;
 
-    fetch('https://gtsofastackoverflowlite.herokuapp.com/api/v2/auth/register', {
+    fetch('http://127.0.0.1:5000/auth/register', {
         method: 'POST',
         headers: {
-            'Accept': 'application/json',
             'Content-type': 'application/json'
         },
         body: JSON.stringify({username:username, email:email, password:password, 
             confirm_password:confirm_password})
     })
-    .then((respon) => {
-        status_code = respon.status
-        return respon.json()
-    })
-    .then((data) => {
-        // if response data is success
-        if (data.status_code == 201){
-            alert(respon.Message)
-            window.location.href = 'log_in.html';
-        }
-        // if response data is not success
-        if (data.status_code == 400){
-            alert(respon.Message)
-        }
-        console.log(respon.Message)
-    })
-    .catch((err)=> console.log(err))
+    .then(respon => respon.json())
+    .then(response => console.log('Success:', JSON.stringify(response)))
+    .catch(error => console.log('Error:', error));
 
     
 }
 
 // create log in function
-document.getElementById('login').addEventListener('submit', logIn);
 function logIn(){
     let username = document.getElementById('username').value;
     let password = document.getElementById('password').value;
 
-    fetch('https://gtsofastackoverflowlite.herokuapp.com/api/v2/auth/login', {
+    fetch('http://127.0.0.1:5000/auth/login', {
         method: 'POST',
         headers: {
             'Accept': 'application/json',
