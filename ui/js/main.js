7307ba91de7e0ce8abe0270b686b4a6e74e86ff7
gtsofa <jlsnyule@yahoo.com>
2018-09-19 05:22:10 +0300

    [chore] fix errors

--- ui/js/main.js
+++ ui/js/main.js
@@ -1,31 +1,51 @@
-// eventListener to handle submit button on sign up form
-document.getElementById('signUp').addEventListener('submit', signUp);
-// document.getElementById('logIn').addEventListener('submit', logIn);
+var registerform = document.getElementById('signup-form')
 
-
-
-// create sign up function
-function signUp(event){
+registerform.addEventListener('submit', function(event) {
     event.preventDefault();
-    let username = document.getElementById('username').value;
-    let email = document.getElementById('email').value;
-    let password = document.getElementById('password').value;
-    let confirm_password = document.getElementById('confirm_password').value;
-
-    fetch('http://127.0.0.1:5000/auth/register', {
+    
+    const username = document.getElementById('username').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const confirm_password = document.getElementById('confirm_password').value;
+    
+    let signup_data = {
+        username: username,
+        email:username,
+        password:password,
+        confirm_password:confirm_password
+        
+    };
+    
+    // let prefixUrl = 'http://127.0.0.1:5000/api/v2';
+    
+    fetch('http://127.0.0.1:5000/api/v2/auth/register', {
         method: 'POST',
         headers: {
+            "Access-Control-Allow-Origin": "*",
             'Content-type': 'application/json'
         },
-        body: JSON.stringify({username:username, email:email, password:password, 
-            confirm_password:confirm_password})
+        body: JSON.stringify(signup_data)
     })
-    .then(respon => respon.json())
-    .then(response => console.log('Success:', JSON.stringify(response)))
-    .catch(error => console.log('Error:', error));
+    .then((respon) => {
+        status_code = respon.status
+        return respon.json()
+    })
+    .then((data) => {
+        // if response data is success
+        if (data.status_code === 200){
+            alert(respon.Message)
+            window.location.href = 'log_in.html';
+        }
+        // if response data is not success
+        if (data.status_code == 400){
+            alert(respon.Message)
+        }
+        console.log(respon.Message)
+    })
+    .catch((err)=> console.log(err))
 
     
-}
+});
 
 // create log in function
 function logIn(){
