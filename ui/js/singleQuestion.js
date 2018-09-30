f89beba7faedc096a5f3b3f9f0cd92254e63fdb8
gtsofa <jlsnyule@yahoo.com>
2018-09-30 13:56:05 +0300

    [feature #160874486] Answer a question A logged in user can answer a question on the app

--- ui/js/singleQuestion.js
+++ ui/js/singleQuestion.js
@@ -30,7 +30,6 @@ function getSingleQuestion(event) {
         <p>${data.question_desc}</p>
             <button class="votes" id="upvote">&#8593; upvote</button>
             <button class="votes" id="downvote">&#8595; downvote</button>
-            <button class="votes" id="deletequestion">&#10008; delete</button>
         <p>Posted on ${data.date_created} by <a href="#">${data.user_id}</a></p>
     </td>
   </tr>
@@ -44,8 +43,7 @@ function getSingleQuestion(event) {
             </div> -->
             <p>The fact remains loreps sim ret jst is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The fact remains loreps sim ret jst is a long established fact that goes home.</p>
             
-            <button class="votes" id="updateanswer">&#9998; update answer</button>
-            <button class="votes" id="acceptanswer">&#10004; Accept Answer</button>
+            
             <p>Answered on ${data.date_created} by <a href="#">${data.user_id}</a></p>
             <p class="answer" style="color:#3498db;"> <a href="#" id="comment">add a comment</a></p>
         </td>
@@ -56,3 +54,34 @@ function getSingleQuestion(event) {
     })
     .catch(error => console.log('Error', error))
 }
+
+let answer = document.getElementById('postAnswer')
+answer.addEventListener('submit', postAnswer);
+// post answer to a question
+function postAnswer(event){
+    event.preventDefault();
+
+    const questionId = window.localStorage.getItem('question_id')
+    let token = window.localStorage.getItem('token')
+
+    // post answer request to a single question
+    fetch('http://127.0.0.1:5000/api/v2/questions/'+questionId+'/answers', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-type': 'application/json',
+            'x-access-token': token
+        },
+        body: JSON.stringify({
+            answer_text: document.getElementById('answerText').value
+        })
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        redirect: window.location.replace("#")
+    })
+    .catch(error => console.log('Error', error))
+
+}
+// event handlers
